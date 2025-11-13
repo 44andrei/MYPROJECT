a4ab0bcbdd011afc0cb47dac897bce3c28efa330
Popov Andrei <popovandrei444@gmail.com>
2025-11-13 12:59:55 +0200

    final comit

---
+++ src/test/java/org/example/test/Basic.java
@@ -0,0 +1,21 @@
+package org.example.test;
+
+import org.example.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
+public class Basic {
+    public WebDriver driver;
+    @BeforeClass
+    public void beferoTest() {
+        driver = Driver.getAutoLocalDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void afterTest() {
+        driver.quit();
+    }
+}
